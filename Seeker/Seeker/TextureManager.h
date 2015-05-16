5581c56e79457a25ff441c0b1f213a63d0331fe0
Gai Takakura <gixa.aturakka@gmail.com>
2015-05-16 17:14:14 +0900

    Sync失敗分

---
+++ Seeker/Seeker/TextureManager.h
@@ -0,0 +1,18 @@
+//----------------------------------------------------------------------
+// TextureManagerƒNƒ‰ƒX
+//----------------------------------------------------------------------
+#pragma once
+#include <list>
+using namespace std;
+
+class TextureManager
+{
+private:
+	list<int> loaded_image;
+
+public:
+	virtual ~TextureManager(){}
+
+public:
+
+};
