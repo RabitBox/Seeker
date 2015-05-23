73b7dd99605aea7558e0c7afa945d44cafb034fd
Gai Takakura <gixa.aturakka@gmail.com>
2015-05-23 15:21:49 +0900

    テクスチャデータの整理

--- Seeker/Seeker/DxRaps.cpp
+++ Seeker/Seeker/DxRaps.cpp
@@ -11,6 +11,9 @@ VECTOR AVGetRap(const AVector3 &vectors)
 
 void DrawQuadPolygon(Vector3 scale, Vector3 position, AVector3 rotation, int graph_handle, int trans_flag)
 {
+	//回転してから中央座標を適用してバーテックスに適用する
+	float x = scale.x, y = scale.y, z = scale.z;
+	WORD index[6] = { 0, 1, 2, 3, 2, 1 };
 	//DrawPolygonIndexed3D(vertex, 4, index, 2, graph_handle, trans_flag);
 	return;
 }
--- Seeker/Seeker/DxRaps.h
+++ Seeker/Seeker/DxRaps.h
@@ -2,5 +2,6 @@
 #include "DxLib.h"
 #include "Generic.h"
 
-VECTOR VGetRap(const Vector3 &vectors);
-VECTOR AVGetRap(const Vector3 &vectors);
+VECTOR	VGetRap(const Vector3 &vectors);
+VECTOR	AVGetRap(const Vector3 &vectors);
+void	DrawQuadPolygon(Vector3 scale, Vector3 position, AVector3 rotation, int graph_handle, int trans_flag);
